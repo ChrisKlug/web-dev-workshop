addb99db6ca6e7d108e08eb7e49a96787241d59c
Carolin Nygren <carolin.nygren@activesolution.se>
2025-09-30 13:55:46 +0200

    lab 5 nonworking

---
+++ labs/lab5/complete/WebDevWorkshop/WebDevWorkshop.Services.Products/Program.cs
@@ -0,0 +1,26 @@
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using WebDevWorkshop.Services.Products.Data;
+using static WebDevWorkshop.Services.Products.Data.IProducts;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<IProducts, EfProducts>();
+builder.AddSqlServerDbContext<ProductsContext>("WebDevWorkshop");
+builder.Services.AddFastEndpoints();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    using (var ctx = scope.ServiceProvider.GetRequiredService<ProductsContext>())
+    {
+        var sql = File.ReadAllText("Data/SeedData.sql");
+        ctx.Database.Migrate();
+    }
+}
+
+app.UseFastEndpoints();
+
+app.Run();
---
+++ labs/lab5/complete/WebDevWorkshop/WebDevWorkshop.Web/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpForwarderWithServiceDiscovery();
+
+builder.AddServiceDefaults();
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+app.Map("/api/{**catch-all}", (HttpContext ctx) => {
+    ctx.Response.StatusCode = 404;
+});
+app.MapForwarder("/{**catch-all}", "https+http://ui");
+
+app.Run();
