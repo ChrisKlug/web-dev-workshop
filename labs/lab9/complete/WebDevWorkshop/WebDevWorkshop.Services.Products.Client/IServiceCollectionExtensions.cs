4026a49a9d9666f75db45f047347351453d16221
Carolin Nygren <carolin.nygren@activesolution.se>
2025-10-01 00:51:04 +0200

    Labb 9

---
+++ labs/lab9/complete/WebDevWorkshop/WebDevWorkshop.Services.Products.Client/IServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WebDevWorkshop.Services.Products.Client
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddProductsClient(this IServiceCollection services, string baseAddress)
+        {
+            services.AddHttpClient<IProductsClient, HttpProductsClient>(client => {
+                client.BaseAddress = new Uri(baseAddress);
+            });
+            return services;
+        }
+    }
+}
