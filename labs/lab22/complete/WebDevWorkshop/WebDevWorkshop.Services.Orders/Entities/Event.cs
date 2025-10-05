3482a144702929f6d90234158aa9524b832e46c0
Carolin Nygren <carolin.nygren@activesolution.se>
2025-10-05 23:10:27 +0200

    labb 22 fungerande

---
+++ labs/lab22/complete/WebDevWorkshop/WebDevWorkshop.Services.Orders/Entities/Event.cs
@@ -0,0 +1,22 @@
+﻿namespace WebDevWorkshop.Services.Orders.Entities
+{
+    internal class Event
+    {
+        public int Id { get; set; }
+        public EventType Type { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public string Data { get; set; } = string.Empty;
+        public EventState State { get; set; }
+    }
+
+    public enum EventType
+    {
+        OrderCreated
+    }
+
+    public enum EventState
+    {
+        Pending,
+        Handled
+    }
+}
---
+++ labs/lab22/complete/WebDevWorkshop/WebDevWorkshop.Services.Orders/Observability/OrdersMetrics.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace WebDevWorkshop.Services.Orders.Observability
+{
+    public class OrdersMetrics
+    {
+        public const string MeterName = "WebDevWorkshop.Services.Orders";
+
+        public OrdersMetrics(IMeterFactory meterFactory)
+        {
+            var meter = meterFactory.Create(MeterName);
+            TotalOrdersCounter = meter.CreateCounter<int>("total_orders");
+        }
+
+        public void AddOrder() => TotalOrdersCounter.Add(1);
+
+        private Counter<int> TotalOrdersCounter { get; }
+    }
+}
